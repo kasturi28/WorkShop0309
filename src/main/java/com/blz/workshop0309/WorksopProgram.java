f623cbd3acd9d232f719a202ab3e40dfc2306cf8
kasturi28 <kasturipansare2818@gmail.com>
2022-09-03 21:40:58 +0530

    Refactored_inserted data into database and displaying it into MYSQL using switch case

--- src/main/java/com/blz/workshop0309/WorksopProgram.java
+++ src/main/java/com/blz/workshop0309/WorksopProgram.java
@@ -4,9 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class WorksopProgram {
-	
+
 	public static void main(String[] args) {
 		/*
 		 * Defining url of database, username & password for JDBC
@@ -17,46 +18,55 @@ public class WorksopProgram {
 
 		try {
 			/*
-			 * defining connections and preparedstatement
-			 * Taking inputs from the user for ID=2
-			 * Displaying in MySQL
-			 * Printing date from SQL for ID=1
-			 * If any improper inputs given by user then throw exception
+			 * defining connections and preparedstatement Taking inputs from the user for
+			 * ID=2 Displaying in MySQL Printing date from SQL for ID=1 If any improper
+			 * inputs given by user then throw exception
 			 */
 			Connection connection = DriverManager.getConnection(dbUrl, username, password);
-//			PreparedStatement prep = connection.prepareStatement(
-//					"insert into AddressBook03 (ID,name,address,city,state,zip,phonenumber) values (?,?,?,?,?,?,?)");
-//			prep.setInt(1, 2);
-//			prep.setString(2, "Dhanashree");
-//			prep.setString(3, "Seawoods");
-//			prep.setString(4, "Thane");
-//			prep.setString(5, "Maharashtra");
-//			prep.setInt(6, 951236);
-//			prep.setDouble(7, 865245164);
-//			prep.executeUpdate();
-			
-//			 PreparedStatement prep1 = connection.prepareStatement("select * from AddressBook03 where ID = ?");
-//             prep1.setInt(1, 1);
-//             ResultSet resultSet = prep1.executeQuery();
-//             while (resultSet.next()) {
-//               System.out.println(resultSet.getInt("ID"));
-//               System.out.println(resultSet.getString("name"));
-//               System.out.println(resultSet.getString("address"));
-//               System.out.println(resultSet.getString("city"));
-//               System.out.println(resultSet.getString("state"));
-//               System.out.println(resultSet.getInt("zip"));
-//               System.out.println(resultSet.getDouble("phonenumber"));
-			
-			/*
-			 * Updating the existing data from the database
-			 * Using MYSQL command for updation
-			 */
-			PreparedStatement prep2 = connection.prepareStatement("update AddressBook03 set zip = '125896' where ID='1'");
+		
+			System.out.println("Enter your choice");
+			System.out.println("1. create data into database");
+			System.out.println("2. print the data from database");
+			System.out.println("3.Update the data from database");
+			Scanner sc = new Scanner(System.in);
+			int i  = sc.nextInt();
+			switch (i) {
+			case 1: {
+				PreparedStatement prep = connection.prepareStatement(
+						"insert into AddressBook03 (ID,name,address,city,state,zip,phonenumber) values (?,?,?,?,?,?,?)");
+				prep.setInt(1, 4);
+				prep.setString(2, "Dhana");
+				prep.setString(3, "Seawoods");
+				prep.setString(4, "Thane");
+				prep.setString(5, "MH");
+				prep.setInt(6, 94178);
+				prep.setDouble(7, 865278412);
+				prep.executeUpdate();
+				System.out.println("Inserted the data successfully!!!");
+			}
+			case 2: {
+				PreparedStatement prep1 = connection.prepareStatement("select * from AddressBook03");
+				ResultSet resultSet = prep1.executeQuery();
+				while (resultSet.next()) {
+					System.out.println(resultSet.getInt("ID"));
+					System.out.println(resultSet.getString("name"));
+					System.out.println(resultSet.getString("address"));
+					System.out.println(resultSet.getString("city"));
+					System.out.println(resultSet.getString("state"));
+					System.out.println(resultSet.getInt("zip"));
+					System.out.println(resultSet.getDouble("phonenumber"));
+				}
+			}
+			case 3: {
+				/*
+				 * Updating the existing data from the database Using MYSQL command for updation
+				 */
+			PreparedStatement prep2 = connection.prepareStatement("update AddressBook03 set zip = '257894' where ID='1'");
 			
 			prep2.executeUpdate();
-
-           }
-		catch (Exception e) {
+			}
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
